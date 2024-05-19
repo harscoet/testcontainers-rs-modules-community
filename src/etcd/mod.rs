b86bedb188c79edc0dbc311d83e42aaddd76bb01
harscoet <donovan.harscoet@gmail.com>
2024-05-19 12:42:44 +0200

    etcd allow custom image name/tag

--- src/etcd/mod.rs
+++ src/etcd/mod.rs
@@ -1,9 +1,61 @@
 use testcontainers::{core::WaitFor, Image, ImageArgs};
 
-const NAME: &str = "quay.io/coreos/etcd";
-const TAG: &str = "v3.5.13";
+const DEFAULT_IMAGE_NAME: &str = "gcr.io/etcd-development/etcd"; // etcd uses gcr.io/etcd-development/etcd as a primary container registry, and quay.io/coreos/etcd as secondary. https://github.com/etcd-io/etcd/blob/main/CHANGELOG/CHANGELOG-3.3.md#other-1
+const DEFAULT_IMAGE_TAG: &str = "v3.5.13"; // https://github.com/etcd-io/etcd/tags
 
-const ETCD_PORT: u16 = 2379;
+pub const ETCD_PORT: u16 = 2379;
+
+/// Module to work with [`etcd`] inside of tests.
+///
+/// This module is based on the official [`etcd docker image`].
+///
+/// # Example
+/// ```
+/// use testcontainers_modules::{etcd, testcontainers::runners::SyncRunner};
+///
+/// let etcd = etcd::Etcd::default().start();
+/// let http_port = etcd.get_host_port_ipv4(etcd::ETCD_PORT);
+///
+/// // do something with the started etcd instance..
+/// ```
+///
+/// [`etcd`]: https://etcd.io/
+/// [`etcd configuration`]: https://etcd.io/docs/v3.5/op-guide/configuration/#command-line-flags
+/// [`etcd docker image`]: https://gcr.io/etcd-development/etcd
+/// [`etcd docker guide`]: https://etcd.io/docs/v3.5/op-guide/container/
+#[derive(Debug)]
+pub struct Etcd {
+    name: String,
+    tag: String,
+}
+
+impl Default for Etcd {
+    fn default() -> Self {
+        Self {
+            name: DEFAULT_IMAGE_NAME.to_owned(),
+            tag: DEFAULT_IMAGE_TAG.to_owned(),
+        }
+    }
+}
+
+impl Etcd {
+    pub fn new<T: Into<String>>(tag: T) -> Self {
+        Self {
+            tag: tag.into(),
+            ..Default::default()
+        }
+    }
+
+    pub fn name(mut self, name: impl Into<String>) -> Self {
+        self.name = name.into();
+        self
+    }
+
+    pub fn tag(mut self, tag: impl Into<String>) -> Self {
+        self.tag = tag.into();
+        self
+    }
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct EtcdArgs;
@@ -23,18 +75,15 @@ impl ImageArgs for EtcdArgs {
     }
 }
 
-#[derive(Debug)]
-pub struct Etcd;
-
 impl Image for Etcd {
     type Args = EtcdArgs;
 
     fn name(&self) -> String {
-        NAME.to_owned()
+        self.name.clone()
     }
 
     fn tag(&self) -> String {
-        TAG.to_owned()
+        self.tag.clone()
     }
 
     fn ready_conditions(&self) -> Vec<WaitFor> {
@@ -50,8 +99,38 @@ mod tests {
     use crate::etcd;
 
     #[tokio::test]
-    async fn etcd_put_get() {
-        let node = etcd::Etcd.start().await;
+    async fn etcd_default() {
+        let etcd = etcd::Etcd::default();
+        assert_eq!(etcd.name, etcd::DEFAULT_IMAGE_NAME);
+        assert_eq!(etcd.tag, etcd::DEFAULT_IMAGE_TAG);
+
+        etcd_put_get(etcd).await;
+    }
+
+    #[tokio::test]
+    async fn etcd_custom_image_tag() {
+        let image_tag: &str = "v3.4.10";
+
+        let etcd = etcd::Etcd::new(image_tag);
+        assert_eq!(etcd.name, etcd::DEFAULT_IMAGE_NAME);
+        assert_eq!(etcd.tag, image_tag);
+
+        etcd_put_get(etcd).await;
+    }
+
+    #[tokio::test]
+    async fn etcd_custom_image_name() {
+        let image_name: &str = "quay.io/coreos/etcd";
+
+        let etcd = etcd::Etcd::default().name(image_name);
+        assert_eq!(etcd.name, image_name);
+        assert_eq!(etcd.tag, etcd::DEFAULT_IMAGE_TAG);
+
+        etcd_put_get(etcd).await;
+    }
+
+    async fn etcd_put_get(etcd: etcd::Etcd) {
+        let node = etcd.start().await;
         let host_ip = node.get_host().await;
         let host_port = node.get_host_port_ipv4(etcd::ETCD_PORT).await;
 
